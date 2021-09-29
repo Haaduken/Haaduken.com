dd3cb866a7549bb892289a5f21299c5686dbc5ec
Haadi <haadi.majeed99@gmail.com>
2021-09-29 00:33:05 -0500

    reformatting code pt2

--- components/Container.js
+++ components/Container.js
@@ -1,97 +1,112 @@
-import React from 'react'
-import {
-    useColorMode,
-    Button,
-    Flex,
-    Box
-} from '@chakra-ui/react'
-import NextLink from 'next/link'
-import styled from '@emotion/styled'
+import { Box, Button, Flex, useColorMode } from "@chakra-ui/react";
 
-import DarkModeSwitch from '../components/DarkModeSwitch'
-import Footer from './Footer'
+import DarkModeSwitch from "../components/DarkModeSwitch";
+import Footer from "./Footer";
+import NextLink from "next/link";
+import React from "react";
+import styled from "@emotion/styled";
 
 const Container = ({ children }) => {
-    const { colorMode } = useColorMode()
+  const { colorMode } = useColorMode();
 
-    const bgColor = {
-        light: 'white',
-        dark: '#171717'
-    }
+  const bgColor = {
+    light: "white",
+    dark: "#171717",
+  };
 
-    const color = {
-        light: 'black',
-        dark: 'white'
-    }
+  const color = {
+    light: "black",
+    dark: "white",
+  };
 
-    const navHoverBg = {
-        light: 'gray.600',
-        dark: 'gray.300',
-    }
+  const navHoverBg = {
+    light: "gray.600",
+    dark: "gray.300",
+  };
 
-    const StickyNav = styled(Flex)`
-        position: sticky;
-        z-index: 10;
-        top: 0;
-        backdrop-filter: saturate(180%) blur(20px);
-        transition: height .5s, line-height .5s;
-        `
+  const StickyNav = styled(Flex)`
+    position: sticky;
+    z-index: 10;
+    top: 0;
+    backdrop-filter: saturate(180%) blur(20px);
+    transition: height 0.5s, line-height 0.5s;
+  `;
 
-    return (
-        <>
-            <StickyNav
-                flexDirection="row"
-                justifyContent="space-between"
-                alignItems="center"
-                maxWidth="800px"
-                minWidth="356px"
-                width="100%"
-                bg={bgColor[colorMode]}
-                as="nav"
-                px={[2, 6, 6]}
-                py={2}
-                mt={8}
-                mb={[0, 0, 8]}
-                mx="auto"
+  return (
+    <>
+      <StickyNav
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        maxWidth="800px"
+        minWidth="356px"
+        width="100%"
+        bg={bgColor[colorMode]}
+        as="nav"
+        px={[2, 6, 6]}
+        py={2}
+        mt={8}
+        mb={[0, 0, 8]}
+        mx="auto"
+      >
+        <Box>
+          <NextLink href="/" passHref>
+            <Button
+              as="a"
+              variant="ghost"
+              p={[1, 2, 4]}
+              _hover={{ backgroundColor: navHoverBg[colorMode] }}
             >
-                <Box>
-                    <NextLink href="/" passHref>
-                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
-                            Home
-                        </Button>
-                    </NextLink>
-                    <NextLink href="/aboutme" passHref>
-                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
-                            About Me
-                        </Button>
-                    </NextLink>
-                    <NextLink href="/portfolio" passHref>
-                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
-                            Portfolio
-                        </Button>
-                    </NextLink>
-                    <NextLink href="/blog" passHref>
-                        <Button as="a" variant="ghost" p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
-                            Blog
-                        </Button>
-                    </NextLink>
-                </Box>
-                <DarkModeSwitch />
-            </StickyNav >
-            <Flex
-                as="main"
-                justifyContent="center"
-                flexDirection="column"
-                bg={bgColor[colorMode]}
-                color={color[colorMode]}
-                px={[0, 4, 4]}
-                mt={[4, 8, 8]}
+              Home
+            </Button>
+          </NextLink>
+          <NextLink href="/aboutme" passHref>
+            <Button
+              as="a"
+              variant="ghost"
+              p={[1, 2, 4]}
+              _hover={{ backgroundColor: navHoverBg[colorMode] }}
             >
-                {children}
-                <Footer/>
-            </Flex>
-        </>
-    )
-}
+              About Me
+            </Button>
+          </NextLink>
+          <NextLink href="/portfolio" passHref>
+            <Button
+              as="a"
+              variant="ghost"
+              p={[1, 2, 4]}
+              _hover={{ backgroundColor: navHoverBg[colorMode] }}
+            >
+              Portfolio
+            </Button>
+          </NextLink>
+          <NextLink href="/blog" passHref>
+            <Button
+              as="a"
+              variant="ghost"
+              p={[1, 2, 4]}
+              _hover={{ backgroundColor: navHoverBg[colorMode] }}
+            >
+              Blog
+            </Button>
+          </NextLink>
+        </Box>
+        <DarkModeSwitch />
+      </StickyNav>
+      <Flex
+        as="main"
+        justifyContent="center"
+        flexDirection="column"
+        bg={bgColor[colorMode]}
+        color={color[colorMode]}
+        px={[0, 4, 4]}
+        mt={[4, 8, 8]}
+      >
+        {children}
+        <Footer />
+      </Flex>
+    </>
+  );
+};
 
-export default Container
+export default Container;
--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,14 @@
-import React,{Component} from "react";
+import { Business, Code, Email, GitHub } from "@material-ui/icons";
+import { Flex, IconButton, Link } from "@chakra-ui/react";
+import React, { Component } from "react";
+
 import ReactDOM from "react-dom";
-import { Link, Flex, IconButton } from "@chakra-ui/react";
 import { requirePropFactory } from "@material-ui/core";
-import { Code, Business, Email, GitHub } from '@material-ui/icons'
 
 // class Footer extends React.Component{
 //     componentDidMount () {
 //         const script = document.createElement("script");
-//         
+//
 //         script.async = true;
 //         document.body.appendChild(script);
 //         return (
@@ -19,30 +20,25 @@ import { Code, Business, Email, GitHub } from '@material-ui/icons'
 // }
 
 const Footer = () => (
-    <Flex justify="center">
-        {/* <script src="lib/getGitInfo.js"></script> */}
-        <div id="gitTime"></div>
-      <Link href="https://github.com/GhastlyFlame" title="GitHub" isExternal>
-        <IconButton
-          aria-label="Github"
-          size="lg"
-          color="gray.500"
-          variant="ghost"
-        >
-            <GitHub fontSize="small"/>
-        </IconButton>
-      </Link>
-      <Link href="mailto:haadi.majeed99@gmail.com" title="Email" isExternal>
-        <IconButton
-          aria-label="Email"
-          size="lg"
-          color="gray.500"
-          variant="ghost"
-        >
-            <Email fontSize="small"/>
-        </IconButton>
-      </Link>
-    </Flex>
+  <Flex justify="center">
+    {/* <script src="lib/getGitInfo.js"></script> */}
+    <div id="gitTime"></div>
+    <Link href="https://github.com/GhastlyFlame" title="GitHub" isExternal>
+      <IconButton
+        aria-label="Github"
+        size="lg"
+        color="gray.500"
+        variant="ghost"
+      >
+        <GitHub fontSize="small" />
+      </IconButton>
+    </Link>
+    <Link href="mailto:haadi.majeed99@gmail.com" title="Email" isExternal>
+      <IconButton aria-label="Email" size="lg" color="gray.500" variant="ghost">
+        <Email fontSize="small" />
+      </IconButton>
+    </Link>
+  </Flex>
 );
 
 export default Footer;
